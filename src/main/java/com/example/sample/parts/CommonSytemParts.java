1dd677de3a48ceb2abe054594c55d44319bb08fa
Kohei Kato <kohe5510@gmail.com>
2020-02-01 22:51:10 +0900

    削除

--- src/main/java/com/example/sample/parts/CommonSytemParts.java
+++
@@ -1,6 +0,0 @@
-package com.example.sample.parts;
-
-public interface CommonSytemParts extends DateFactory{
-
-	
-}
--- src/main/java/com/example/sample/web/domain/SampleService.java
+++
@@ -1,5 +0,0 @@
-package com.example.sample.web.domain;
-
-public interface SampleService {
-	public OutputData execute(InputData input);
-}
