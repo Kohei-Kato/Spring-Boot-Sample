35dfe063a9c34957abecc012abe64ef9d392289f
Kohei Kato <kohe5510@gmail.com>
2020-03-24 01:26:51 +0900

    メッセージ管理を追加.

--- src/main/java/com/example/sample/web/api1/application/SampleControler.java
+++ src/main/java/com/example/sample/web/api1/application/SampleControler.java
@@ -1,6 +1,8 @@
 package com.example.sample.web.api1.application;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +15,15 @@ import com.example.sample.web.api1.domain.SampleService;
 import com.example.sample.web.api1.domain.dto.InputDto;
 import com.example.sample.web.api1.domain.dto.OutputDto;
 
+import java.util.Locale;
+
 import org.dozer.Mapper;
 
 @RestController
 public class SampleControler {
+	
+	@Autowired
+	private MessageSource messageSource;
 
 	@Autowired
 	private SampleService sampleService;
@@ -42,13 +49,13 @@ public class SampleControler {
 			mapper.map(outputData, outputForm);
 			outputForm.setResult(true);
 			outputForm.setResultCode("0");
-			outputForm.setMessage("SampleAPIが正常終了しました。");
+			outputForm.setMessage(messageSource.getMessage("app.message.sample.normal.001", null, Locale.JAPAN));
 			
 		} else {
 			// Serviceが異常終了した場合
 			outputForm.setResult(false);
 			outputForm.setResultCode("1");
-			outputForm.setMessage("SampleAPIが異常終了しました。");
+			outputForm.setMessage(messageSource.getMessage("app.message.sample.err.001", null, Locale.JAPAN));
 			
 		}
 		
