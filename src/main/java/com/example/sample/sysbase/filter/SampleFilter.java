74c582b06a9bb43ad0a61751c22f36012d890ce8
Kohei Kato <kohe5510@gmail.com>
2020-03-27 07:56:14 +0900

    ログ制御　MDCとしてUUIDの埋め込みを追加.

--- src/main/java/com/example/sample/sysbase/filter/SampleFilter.java
+++ src/main/java/com/example/sample/sysbase/filter/SampleFilter.java
@@ -2,9 +2,11 @@ package com.example.sample.sysbase.filter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 import javax.servlet.*;
 import java.io.IOException;
+import java.util.UUID;
 
 /**
  *　フィルタサンプル.
@@ -13,10 +15,22 @@ public class SampleFilter implements Filter {
 
     private Logger logger = LoggerFactory.getLogger(SampleFilter.class);
 
+    final String KEY_UUID = "UUID";
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        MDC.put(KEY_UUID, createUUID());
+
         logger.info("フィルタ１ start.");
+
         chain.doFilter(request, response);
-        logger.info("フィルタ１ end.");
+
+        logger.info("KEY_UUID end.");
+
+        MDC.remove(KEY_UUID);
+    }
+
+    private String createUUID(){
+        return UUID.randomUUID().toString();
     }
 }
